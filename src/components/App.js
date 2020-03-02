0db9f94d33b5a51f4d0bb46a31f55593bbf3e57b
Shin Woo Lee <vriz@vriz.net>
2020-03-02 22:53:49 +0900

    1.4. Installing React Router and Declaring Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+import Home from './Home';
+import About from './About';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
