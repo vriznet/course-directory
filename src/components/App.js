b91d6e21d67e625abc219b8f4a77739e801bff08
Shin Woo Lee <vriz@vriz.net>
2020-03-03 00:11:31 +0900

    1.5. Inline Rendering with <Route>

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
+      <Route path="/about" render={() => <About title="About" />} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
     </div>
   </BrowserRouter>
 );
