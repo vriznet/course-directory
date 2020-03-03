d3bbc594e7f3ead08b8772f732d4de44b53716ca
Shin Woo Lee <vriz@vriz.net>
2020-03-03 18:11:02 +0900

    2.4. Redirecting a Route

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Redirect } from 'react-router-dom';
 import HTML from './courses/HTML';
 import CSS from './courses/CSS';
 import JavaScript from './courses/JavaScript';
@@ -20,6 +20,11 @@ const Courses = () => (
         </li>
       </ul>
     </div>
+    <Route
+      exact
+      path="/courses"
+      render={() => <Redirect to="/courses/html" />}
+    />
     <Route path="/courses/html" component={HTML} />
     <Route path="/courses/css" component={CSS} />
     <Route path="/courses/javascript" component={JavaScript} />
