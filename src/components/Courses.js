ae7cc0e593e9a4b34c5c051aeaf71ac00cf0bfc2
Shin Woo Lee <vriz@vriz.net>
2020-03-03 18:23:00 +0900

    2.5. Using the Match Object

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -4,30 +4,30 @@ import HTML from './courses/HTML';
 import CSS from './courses/CSS';
 import JavaScript from './courses/JavaScript';
 
-const Courses = () => (
+const Courses = ({ match }) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
         <li>
-          <NavLink to="/courses/html">HTML</NavLink>
+          <NavLink to={`${match.url}/html`}>HTML</NavLink>
         </li>
         <li>
-          <NavLink to="/courses/css">CSS</NavLink>
+          <NavLink to={`${match.url}/css`}>CSS</NavLink>
         </li>
         <li>
-          <NavLink to="/courses/javascript">JavaScript</NavLink>
+          <NavLink to={`${match.url}/javascript`}>JavaScript</NavLink>
         </li>
       </ul>
     </div>
     <Route
       exact
-      path="/courses"
-      render={() => <Redirect to="/courses/html" />}
+      path={match.path}
+      render={() => <Redirect to={`${match.path}/html`} />}
     />
-    <Route path="/courses/html" component={HTML} />
-    <Route path="/courses/css" component={CSS} />
-    <Route path="/courses/javascript" component={JavaScript} />
+    <Route path={`${match.path}/html`} component={HTML} />
+    <Route path={`${match.url}/css`} component={CSS} />
+    <Route path={`${match.url}/javascript`} component={JavaScript} />
   </div>
 );
 
